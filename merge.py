5a1b156d48bee4020f429903078e7af3f62d5361
Nikolay Korovaiko <korovaikon@gmail.com>
2022-02-19 22:54:03 -0800

    switch to using gettempdir

--- merge.py
+++ merge.py
@@ -4,8 +4,7 @@ import pathlib
 import os
 import json
 
-temp = tempfile.NamedTemporaryFile(mode="w", prefix='comp_')
-temp_dir_path = pathlib.Path(temp.name).parent
+temp_dir_path = tempfile.gettempdir()
 
 def build_commands(temp_dir_path):
     commands = []
@@ -13,15 +12,21 @@ def build_commands(temp_dir_path):
     pattern = f"{temp_prefix}*"
     comp_file_glob = str(temp_dir_path) + os.sep + pattern
     command_file_list = list (glob.glob(comp_file_glob))
+    #print(command_file_list)
     for f in command_file_list:
         commands.extend(read_single_compile_command(f))
 
     return commands
 
 def read_single_compile_command(filename):
+    print(f"loading file {filename}")
     return json.load(open(filename))
 
-if __name__ == "__main__":
+def merge():
     commands_list = build_commands(temp_dir_path)
     with open("compile_commands.json", "w") as compdb:
         json.dump(commands_list, compdb, indent=4, sort_keys=True)
+
+if __name__ == "__main__":
+    merge()
+
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -2,27 +2,31 @@ from os import environ
 import subprocess
 import os
 import glob
-import pathlib
 import tempfile
 import stat
+import sys
+
+
 
 def test_basic():
     environ["PATH"] = f"{os.getcwd()}:{environ['PATH']}"
-    print(environ["PATH"])
     st = os.stat('g++')
     os.chmod('g++', st.st_mode | stat.S_IEXEC)
+
     path_to_gpp = subprocess.check_output(["which", "g++"]).decode("utf-8")
     if os.getcwd() + os.sep + "g++" != path_to_gpp.strip():
         os.environ["PATH"] = f"{os.getcwd()}:{os.environ['PATH']}"
-    print(os.environ["PATH"])
-    rc = subprocess.run(["which", "g++"], env=environ)
-    rc = subprocess.run(["echo", "bla $PATH"], env=environ)
+
     rc = subprocess.run(["g++", "tests/cpp/main.cpp"], env=environ)
     assert rc.returncode == 0
-    temp = tempfile.NamedTemporaryFile(mode="w")
-    temp_dir_path = pathlib.Path(temp.name).parent
+    temp_dir_path = tempfile.gettempdir()
     temp_prefix = os.environ.get("COMPDB_PREFIX", "comp_")
     pattern = f"{temp_prefix}*"
     comp_file_glob = str(temp_dir_path) + os.sep + pattern
     command_file_list = list (glob.glob(comp_file_glob))
     assert(len(command_file_list) == 1)
+
+    sys.path.append(".")
+    import merge
+    merge.merge()
+    

76e058ccbfc550b97340f5002ed4edb2bdecffa7
Nikolay Korovaiko <korovaikon@gmail.com>
2022-02-21 11:29:31 -0800

    flake8 merge.py

--- merge.py
+++ merge.py
@@ -1,32 +1,35 @@
 import tempfile
 import glob
-import pathlib
 import os
 import json
 
+
 temp_dir_path = tempfile.gettempdir()
+
 
 def build_commands(temp_dir_path):
     commands = []
     temp_prefix = os.environ.get("COMPDB_PREFIX", "comp_")
     pattern = f"{temp_prefix}*"
     comp_file_glob = str(temp_dir_path) + os.sep + pattern
-    command_file_list = list (glob.glob(comp_file_glob))
-    #print(command_file_list)
+    command_file_list = list(glob.glob(comp_file_glob))
+    # print(command_file_list)
     for f in command_file_list:
         commands.extend(read_single_compile_command(f))
 
     return commands
 
+
 def read_single_compile_command(filename):
     print(f"loading file {filename}")
     return json.load(open(filename))
+
 
 def merge():
     commands_list = build_commands(temp_dir_path)
     with open("compile_commands.json", "w") as compdb:
         json.dump(commands_list, compdb, indent=4, sort_keys=True)
 
+
 if __name__ == "__main__":
     merge()
-
